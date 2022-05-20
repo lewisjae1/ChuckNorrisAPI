bb63d439c6d027b43fc82c241bd31781a1f163cf
Jaein Lewis <dlwodls0115@gmail.com>
2022-05-19 23:23:32 -0700

    Created winform app

---
+++ ChuckNorrisWinForm/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ChuckNorrisWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btn_joke = new System.Windows.Forms.Button();
+            this.lbl_category = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(158, 124);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(151, 28);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // btn_joke
+            // 
+            this.btn_joke.Location = new System.Drawing.Point(83, 197);
+            this.btn_joke.Name = "btn_joke";
+            this.btn_joke.Size = new System.Drawing.Size(151, 134);
+            this.btn_joke.TabIndex = 2;
+            this.btn_joke.Text = "Generate Joke";
+            this.btn_joke.UseVisualStyleBackColor = true;
+            // 
+            // lbl_category
+            // 
+            this.lbl_category.AutoSize = true;
+            this.lbl_category.Location = new System.Drawing.Point(45, 127);
+            this.lbl_category.Name = "lbl_category";
+            this.lbl_category.Size = new System.Drawing.Size(73, 20);
+            this.lbl_category.TabIndex = 3;
+            this.lbl_category.Text = "Category:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 423);
+            this.Controls.Add(this.lbl_category);
+            this.Controls.Add(this.btn_joke);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Chuck Norris Joke Generator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private ComboBox comboBox1;
+        private Button btn_joke;
+        private Label lbl_category;
+    }
+}
---
+++ ChuckNorrisWinForm/Form1.cs
@@ -0,0 +1,20 @@
+namespace ChuckNorrisWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeComboBox();
+        }
+
+        private void InitializeComboBox()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
